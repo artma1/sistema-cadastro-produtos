f5b00d6bb370b30a52a8a540a8f898d938bd1269
Artur Maciel de Assis Pinto Pinto <739920@sga.pucminas.br>
2024-12-02 12:26:55 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Supplier.cs
@@ -0,0 +1,14 @@
+﻿namespace sistema_vega.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Adress { get; set; }
+        public string CNPJ { get; set; }
+        public string CEP { get; set; } 
+        public string QRCode { get; set; }
+
+    }
+}
