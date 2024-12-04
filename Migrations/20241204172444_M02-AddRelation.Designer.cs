38bfc6d1874ca1cfff0a7902eb865d26890e3b70
Artur Maciel de Assis Pinto Pinto <739920@sga.pucminas.br>
2024-12-04 17:23:22 -0300

    relação pronta

--- Migrations/20241204172444_M02-AddRelation.Designer.cs
+++ Migrations/20241204172444_M02-AddRelation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sistema_vega.Models;
 
@@ -11,9 +12,11 @@ using sistema_vega.Models;
 namespace sistema_vega.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241204172444_M02-AddRelation")]
+    partial class M02AddRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
